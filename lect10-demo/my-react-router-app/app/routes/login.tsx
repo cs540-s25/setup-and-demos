6b085d68795cbc14b064896fc56e786b3792314e
John <Jmr7in108@gmail.com>
2025-02-19 20:02:31 -0500

    one more comment

--- lect10-demo/my-react-router-app/app/routes/login.tsx
+++ lect10-demo/my-react-router-app/app/routes/login.tsx
@@ -9,6 +9,8 @@ export default function Login() {
                 "Content-Type": "application/json"
             }
         }).then(response => response.json()).then(data => setVal(data.value))
-    }, []);
+    }, []); // this second [] argument is important! it means "rerun the function that was the first argument when any of these variables change"
+    // since the list is empty, we will never rerun as long as the component stays rendered
+
     return <h1>I am a Login Page! My server-fetched value is {val}</h1>;
 }
